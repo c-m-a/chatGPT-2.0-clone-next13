2ef306d55978a6600d3698346e0abb1e6d807024
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-25 17:56:34 -0500

    Add: send message logic with firestore functions

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from 'react'
 import { useSession } from 'next-auth/react'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
 
 type Props = {
@@ -12,9 +15,50 @@ export default function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState('')
   const { data: session } = useSession()
 
+  // TODO: useSWR to get model
+  const model = 'text-davinci-003'
+
+  const sendMessage = async (e: Formevent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!prompt) return
+
+    const input = prompt.trim()
+    setPrompt('')
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    }
+
+    await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+      message
+    )
+
+    // Toast notification to say Loading...
+ 
+    await fetch('/api/askQuestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: input, chatId, model, session
+      }),
+    }).then(() => {
+      // Toast notification to say successfull!
+    })
+  }
+
   return <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
     <form
       className="p-5 space-x-5 flex-1 flex"
+      onSubmit={sendMessage}
     >
       <input
         className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
