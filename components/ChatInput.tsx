3b0b61ec5b138eabdee91674918139613353e181
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-25 16:21:37 -0500

    Add: Chat input elements, basic logic and styles

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,4 +1,42 @@
-export default function ChatInput() {
-  return <div>Chat Input</div>
+'use client'
+
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
+
+type Props = {
+  chatId: string
+}
+
+export default function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState('')
+  const { data: session } = useSession()
+
+  return <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+    <form
+      className="p-5 space-x-5 flex-1 flex"
+    >
+      <input
+        className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+        type="text"
+        disabled={!session}
+        value={prompt}
+        onChange={e => setPrompt(e.target.value)}
+        placeholder="Type your message here.."
+      />
+
+      <button
+        type="submit"
+        disabled={!prompt || !session}
+        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+      >
+        <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+      </button>
+    </form>
+
+    <div>
+      {/* ModelSelection */}
+    </div>
+  </div>
 }
 
