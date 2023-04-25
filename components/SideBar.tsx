f24f0f2af449ce6aea810c15af92f4d750619703
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-25 14:36:29 -0500

    Add: Listing chats in the Side Bar component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,20 @@
 'use client'
 
+import { collection, orderBy, query } from 'firebase/firestore'
 import { useSession, signOut } from 'next-auth/react'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '../firebase'
+
+import ChatRow from './ChatRow'
 import NewChat from './NewChat'
 
 export default function SideBar() {
   const { data: session } = useSession()
+  const [chats, loading, error] = useCollection(
+    session && query(
+      collection(db, 'users', session?.user?.email!, 'chats'),
+      orderBy('createdAt', 'asc')
+  ))
 
   return <div className="p-2 flex flex-col h-screen">
     <div className="flex-1">
@@ -13,7 +23,9 @@ export default function SideBar() {
         <div>
           { /* ModelSelection */ }
         </div>
-         { /* Map through the chatRows */ }
+         {chats?.docs.map(chat => (
+          <ChatRow key={chat.id} id={chat.id} />
+         ))}
       </div>
     </div>
     { session && (
