f3aff69cfb9272b6c68f82b1c6e59c03ac75a8c0
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-26 02:07:47 -0500

    Add: getting data from ChatGPT to save it into the corresponding chat

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import query from '../../lib/queryApi'
+import admin from 'firebase-admin'
+import { adminDb } from '../../firebaseAdmin.ts'
 
 type Data = {
   answer: string
@@ -18,8 +21,30 @@ export default async function handler(
 
   if (!chatId) {
     res.status(400).json({ answer: 'Please provide a valid chat ID'})
+    return
   }
 
   // ChatGPT Query
-  const res = await query(prompt, chatId, model)
+  const response = await query(prompt, chatId, model)
+
+  const message: Message = {
+    text: response || 'ChatGPT was unable to find an answer for that!',
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: 'ChatGPT',
+      name: 'ChatGPT',
+      avatar: 'https://links.papareact.com/89k',
+    },
+  }
+
+  await adminDb
+    .collection('users')
+    .doc(session?.user?.email)
+    .collection('chats')
+    .doc(chatId)
+    .collection('messages')
+    .add(message)
+
+  res.status(200).json({ answer: message.text })
 }
+
