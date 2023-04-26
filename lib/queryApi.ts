e09bac89841ba38c7d81f1e042e32ca121acad2d
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-25 23:28:01 -0500

    Fix: variable to prompt

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -3,7 +3,7 @@ import openai from './chatgpt'
 const query = async (prompt: string, chatId: string, model: string) => {
   const res = await openai.createCompletion({
     model,
-    propmt,
+    prompt,
     temperature: 0.9,
     top_p: 1,
     max_token: 1000,
